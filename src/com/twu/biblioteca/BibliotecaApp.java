4afae11b86b0519e12c93b928f159c598ebbf9d8
Dean Coulstock <dcoulstock@qktech.com.au>
2020-04-15 17:40:59 +1000

    Added book list and relevant logic to display book list. Also added method to sleep the program and await input for a better user experience.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,46 @@
 package com.twu.biblioteca;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("-----------PROGRAM ENTRY POINT-----------");
+    private static void pressEnterKeyToContinue()
+    {
+        System.out.println("Press Enter key to view available books...");
+        try
+        {
+            System.in.read();
+        }
+        catch(Exception e)
+        {}
+    }
+
+    public static void displayWelcomeMessage() throws InterruptedException {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        System.out.println();
+        pressEnterKeyToContinue();
+//        Thread.sleep(1500); //Provides a more natural experience to the user. The welcome message
+//                                // will otherwise not be seen due to the long list of books
+    }
+    public static void displayListOfBooks() throws IOException, InterruptedException {
+        System.out.println("Here is a list of books available to hire (alphabetical order by book title):");
+        System.out.println("-----------------------------------------------------------------------------");
+        Thread.sleep(1500);
+        BufferedReader in = new BufferedReader(new FileReader("BookList.txt"));
+        String line;
+        while((line = in.readLine()) != null)
+        {
+            System.out.println(line);
+            Thread.sleep(250);
+        }
+        in.close();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("-----------PROGRAM ENTRY POINT-----------"); //for debugging and thread timing
+        displayWelcomeMessage();
+        BibliotecaApp.displayListOfBooks();
     }
 }
 
