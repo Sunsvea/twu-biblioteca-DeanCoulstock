11e65a7630172fea44843701ee11acb609dd4147
Dean Coulstock <dcoulstock@qktech.com.au>
2020-04-15 16:56:43 +1000

    Final small details before beginning

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,12 @@
 package com.twu.biblioteca;
-
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("-----------PROGRAM ENTRY POINT-----------");
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 }
+
+/*
+* Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"
+* */
