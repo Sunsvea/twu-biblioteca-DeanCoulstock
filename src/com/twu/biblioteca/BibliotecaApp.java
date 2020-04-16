379ff1b0ee1a3eeae2dd963b8f010f99536b7cae
Dean Coulstock <dcoulstock@qktech.com.au>
2020-04-16 11:08:37 +1000

    Implemented displayMainMenu() and phased out pressEnterToContinue() since it is not currently needed.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,26 +2,26 @@ package com.twu.biblioteca;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
-    private static void pressEnterKeyToContinue()
-    {
-        System.out.println("Press Enter key to view available books...");
-        try
-        {
-            System.in.read();
-        }
-        catch(Exception e)
-        {}
-    }
+// %     Phased out due to implementation of main menu. This function may return.    %
+//    private static void pressEnterKeyToContinue()
+//    {
+//        System.out.println("Press Enter key to view available books...");
+//        try
+//        {
+//            System.in.read();
+//        }
+//        catch(Exception e)
+//        {}
+//    }
 
     public static void displayWelcomeMessage() throws InterruptedException {
         System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
         System.out.println();
-        pressEnterKeyToContinue();
-//        Thread.sleep(1500); //Provides a more natural experience to the user. The welcome message
-//                                // will otherwise not be seen due to the long list of books
+//        pressEnterKeyToContinue(); Phased out due to implementation of main menu
     }
     public static void displayListOfBooks() throws IOException, InterruptedException {
         System.out.println("Here is a list of books available to hire (alphabetical order by book title):");
@@ -46,11 +46,28 @@ public class BibliotecaApp {
                 PublishDateList.close();
                 authorList.close();
                 nameList.close(); }
+    public static void displayMainMenu() throws IOException, InterruptedException {
+        String options = "1. List of books";
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please choose an option : ");
+        System.out.println(options);
+        int choice = keyboard.nextInt();
+        do{
+            Thread.sleep(1000);
+
+            if (choice == 1) {
+                displayListOfBooks();
+                break;
+            }
+        }while(!(choice < 1));
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("-----------PROGRAM ENTRY POINT-----------"); //for debugging and thread timing
         displayWelcomeMessage();
-        BibliotecaApp.displayListOfBooks();
+        displayMainMenu();
+
+
     }
 }
 
