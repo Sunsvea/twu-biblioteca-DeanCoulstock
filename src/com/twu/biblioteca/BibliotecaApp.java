8775a30ea0c3495a46ec46a790342687250db1e2
Dean Coulstock <dcoulstock@qktech.com.au>
2020-04-16 10:42:30 +1000

    Added string formatting for columns and by line for authors. Needs code cleanup work

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -27,15 +27,25 @@ public class BibliotecaApp {
         System.out.println("Here is a list of books available to hire (alphabetical order by book title):");
         System.out.println("-----------------------------------------------------------------------------");
         Thread.sleep(1500);
-        BufferedReader in = new BufferedReader(new FileReader("BookList.txt"));
-        String line;
-        while((line = in.readLine()) != null)
-        {
-            System.out.println(line);
-            Thread.sleep(250);
-        }
-        in.close();
-    }
+        BufferedReader nameList = new BufferedReader(new FileReader("BookNames.txt"));
+        BufferedReader authorList = new BufferedReader(new FileReader("BookAuthors.txt"));
+        BufferedReader PublishDateList = new BufferedReader(new FileReader("BookListPublishDates.txt"));
+        String bookName;
+        String bookAuthor;
+        String bookPublishDate;
+
+//        while((bookName = nameList.readLine()) != null) {
+//            while((bookAuthor = authorList.readLine()) != null) {
+                while(((bookPublishDate = PublishDateList.readLine()) != null) &
+                        ((bookAuthor = authorList.readLine()) != null) &
+                        ((bookName = nameList.readLine()) != null)) {
+                    String formattedBookAuthor = "by " + bookAuthor;
+                    System.out.printf("%-35s %-35s %-35s\n", bookName, formattedBookAuthor, bookPublishDate);
+                    Thread.sleep(250);
+                }
+                PublishDateList.close();
+                authorList.close();
+                nameList.close(); }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("-----------PROGRAM ENTRY POINT-----------"); //for debugging and thread timing
